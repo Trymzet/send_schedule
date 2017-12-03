a45a44906751ac5492c55cdecf478d062dcfaaaf
Michał Zawadzki <michalmzawadzki@gmail.com>
2017-12-03 19:35:09 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-TO = "michal.zawadzki@microfocus.com"
+TO = "michal@b.com"
 TODAY = date.today().strftime("%d.%m") # e.g. 25.11
 WDEM_FILE_FORMAT = "xlsx"
 CRC_FILE_FORMAT = "csv"
