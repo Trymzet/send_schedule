9185082f4ebbd68084c5f99b16401ee494b74b6f
Michał Zawadzki <michalmzawadzki@gmail.com>
2017-12-11 21:14:52 +0100

    Added a basic CRC schedule

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 from datetime import date
 
-TO = "michal@b.com"
-TODAY = date.today().strftime("%d.%m") # e.g. 25.11
+TO = "m@xy.com"
+TODAY_STAMP = date.today()
+TODAY = TODAY_STAMP.strftime("%d.%m") # e.g. 25.11
 WDEM_FILE_FORMAT = "xlsx"
 CRC_FILE_FORMAT = "csv"
